572dd606d3f575312bb25edae6b0ab74c61a4edc
eobroets <wilhelm.storen@gmail.com>
2020-03-03 13:30:29 +0100

    l

--- spectrogram_gdal.py
+++ spectrogram_gdal.py
@@ -17,11 +17,13 @@ ds_band3 = np.array(ds.GetRasterBand(3).ReadAsArray())
 
 ds_target1 = np.zeros(len(ds_band1[1000,:]))
 ds_target3 = np.zeros(len(ds_band3[1000,:]))
-for i in range(712,728):
+
+snippet = [712,728]
+for i in range(snippet[0],snippet[1]):
     ds_target1 = ds_band1[i,:] + ds_target1
     ds_target3 = ds_band3[i,:] + ds_target3
-ds_target1 = ds_target1/16
-ds_target3 = ds_target3/16
+ds_target1 = ds_target1/(snippet[1]-snippet[0])
+ds_target3 = ds_target3/(snippet[1]-snippet[0])
 N = len(ds_target1)
 length = N*5 
 fs = N/length
@@ -30,25 +32,32 @@ x = np.linspace(0,length,N)
 xf = np.linspace(0,length*fs/2,N//2)/length
 fy = fftpack.fft(ds_target1)[:N//2]/N
 
-f, t, Z = ss.spectrogram(ds_target1, fs,nperseg=256)
 
+j = ds_target1-ds_target3
+nper = 64
+f, t, Z = ss.spectrogram(ds_target1, fs,nperseg = nper )
+ff, tt, ZZ = ss.spectrogram(j, fs,nperseg = nper)
 
 plt.figure(figsize=(15,10))
-plt.subplot(3,2,1)
+plt.subplot(4,2,1)
 plt.plot(x,np.log(ds_target1)*10)
 plt.plot(x,np.log(ds_target3)*10)
 plt.xlabel("[m]")
 plt.ylabel("[dB]")
-plt.subplot(3,2,2)
+plt.subplot(4,2,2)
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("..")
 plt.plot(xf,np.abs(fy))
-plt.subplot(3,2,3)
-plt.pcolormesh(t,f,np.log(Z)*10)
+plt.subplot(4,2,3)
+plt.pcolormesh(t,f,np.log(Z)*10, vmin=-50,vmax=0)
 plt.ylabel('[Hz]')
 plt.xlabel('[m]')
 plt.colorbar()
-
+plt.subplot(4,2,4)
+plt.pcolormesh(tt,ff,np.log(ZZ)*10,vmin=-50,vmax=0)
+plt.colorbar()
+plt.ylabel('[Hz]')
+plt.xlabel('[m]')
 plt.show()
 
 
@@ -60,13 +69,13 @@ f2 =np.abs( fftpack.fft(ds_target3))
 h  = np.abs(f1*f2)
 
 
-
-
+tp = 2
+plt.subplot(tp,2,1)
 cc = ss.correlate(ds_target1, ds_target3)
 cc  = cc[len(cc)//2:]
 plt.plot(cc)
 #plt.plot(h)
-plt.show()
 
+plt.show()
 
 
