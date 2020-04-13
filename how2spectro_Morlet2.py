a7f8788baaedc94811d7d6aa1ebe2120677d8480
eobroets <wilhelm.storen@gmail.com>
2020-04-13 16:49:40 +0200

    l

--- how2spectro_Morlet2.py
+++ how2spectro_Morlet2.py
@@ -5,7 +5,7 @@
 #{{{ >>> prgm
 #	* [version] derived from -rw-rw-r-- 1 villardl villardl 2,1K mars  29 12:55 how2spectro.py
 #	* [run] 'python how2spectro_Morlet2.py'
-#	* [purpose]{ >>> demo to handle scipy.cwt function compatible with morlet2 (wavelet accounting for ad-hoc frequencies)
+#	* [purpose]{ >>> demo to handle scipy.cwt function compatible with morlet2 (wavelet accounting for ad-hoc frequencies)
 #}}}
 #
 
@@ -127,22 +127,22 @@ def cwt(data, wavelet, widths, dtype=None, **kwargs):
 #>>> observation time config:
 Tobs = 60. # Total observation period (sec)
 fs = 50. # Sampling frequency (Hz)
-N = Tobs/(1/fs) # Nb points
-print N, " <<< Nb of points "
-d1t = np.arange(N)/float(fs) # time axis 
+N = Tobs/(1/fs) # Nb points
+print(N, " <<< Nb of points ")
+d1t = np.arange(N)/float(fs) # time axis 
 
 #
 #>>> input signal config:
-amp = 2*np.sqrt(2) # amplitude
-d1amp = (1.-np.exp(-d1t/(Tobs/5.))) # optional varying amplitude
-f0 = 0.1 # carrier frequency
-#d1mod = (f0/6.)*np.cos(2*np.pi*0.25*d1t) # optional sinusoidal modulation
-d1mod = 2*np.pi*(f0/0.10)*d1t/Tobs # linear modulation
-d1w = 2*np.pi*f0 + d1mod # pulsation
+amp = 2*np.sqrt(2) # amplitude
+d1amp = (1.-np.exp(-d1t/(Tobs/5.))) # optional varying amplitude
+f0 = 0.1 # carrier frequency
+#d1mod = (f0/6.)*np.cos(2*np.pi*0.25*d1t) # optional sinusoidal modulation
+d1mod = 2*np.pi*(f0/0.10)*d1t/Tobs # linear modulation
+d1w = 2*np.pi*f0 + d1mod # pulsation
 d1sgnl = amp*np.sin(d1w*d1t) # input signal 
-d1sgnl *= d1amp # optional increasing amp input signal
+d1sgnl *= d1amp # optional increasing amp input signal
 fMx = f0 + max(d1mod)
-print fMx, " <<< max frequency of the input signal "
+print(fMx, " <<< max frequency of the input signal ")
 
 #
 #>>> add variable noise
@@ -158,6 +158,7 @@ d1sgnl *= d1noise
 
 # input plot  
 fig1 = plt.figure(1)
+plt.title("Plot of signal")
 plt.ylabel('V')
 plt.xlabel('Time [sec]')
 plt.plot(d1t,d1sgnl) 
@@ -165,14 +166,14 @@ fig1.show()
 
 
 #Compute and plot the spectrogram.
-N_sb = int(N/10)  # N points of the sub-window
+N_sb = int(N/10)  # N points of the sub-window
 fig2 = plt.figure(2)
-#d1win = signal.get_window(('hann'), N_sb) # Hanning sliding window
-d1win = signal.get_window(('tukey', 0.3), N_sb) # Tukey sliding window
-d1f_cmp, d1t_cmp, d2PSD = signal.spectrogram(d1sgnl, fs, nperseg=N_sb, window=d1win, noverlap=N_sb/4., axis=-1, mode='psd') #  \n ...
-# d1f_cmp, d1t_cmp, d2PSD : [return] computed frequency and time samples
-# d2PSD : [return] computed PSD according to mode option (PSD expresssed in V**2/Hz voltage per Hertz)
-# ¡¡¡ important options: nperseg & noverlap !!!
+#d1win = signal.get_window(('hann'), N_sb) # Hanning sliding window
+d1win = signal.get_window(('tukey', 0.3), N_sb) # Tukey sliding window
+d1f_cmp, d1t_cmp, d2PSD = signal.spectrogram(d1sgnl, fs, nperseg=N_sb, window=d1win, noverlap=N_sb/4., axis=-1, mode='psd') #  \n ...
+# d1f_cmp, d1t_cmp, d2PSD : [return] computed frequency and time samples
+# d2PSD : [return] computed PSD according to mode option (PSD expresssed in V**2/Hz voltage per Hertz)
+# ¡¡¡ important options: nperseg & noverlap !!!
 
 # spectro plot  
 plt.pcolormesh(d1t_cmp,d1f_cmp, d2PSD)
@@ -184,21 +185,21 @@ fig2.show()
 #Compute & plot cwt based spectro:
 w = 10
 d1f = np.linspace(0., int(fMx)+1, 200)
-print d1f, "<<< ACH-print freq <<<"
+print(d1f, "<<< ACH-print freq <<<")
 widths = w*fs / (2*d1f*np.pi)
-print widths, "<<< ACH-print widths <<<"
+print(widths, "<<< ACH-print widths <<<")
 #cwtm = signal.cwt(d1sgnl, morlet2, widths) #, w=w)
 cwtm = cwt(d1sgnl, morlet2, widths, w=w)
 fig3 = plt.figure(3)
 plt.pcolormesh(d1t, d1f, np.abs(cwtm), cmap='viridis')
-print np.shape(d1t), "<<< ACH-print.shape(d1t), <<<"
-print np.shape(cwtm), "<<< ACH-print size(cwtmatr) <<<"
+print(np.shape(d1t), "<<< ACH-print.shape(d1t), <<<")
+print(np.shape(cwtm), "<<< ACH-print size(cwtmatr) <<<")
 fig3.show()
 #
 
 
-raw_input()
 
-#}}} <<< EoDemo
+
+#}}} <<< EoDemo
 
 
